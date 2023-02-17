60e4199e57a76acab14f68a7664f8e9a0d267bbf
Mrsuper125 <Mrsuper1221@gmail.com>
2023-02-17 18:29:00 +0300

    frontend documentation

--- app/src/main/java/com/example/shoppingcheck_list/ProductAddActivity.kt
+++ app/src/main/java/com/example/shoppingcheck_list/ProductAddActivity.kt
@@ -6,10 +6,18 @@ import android.widget.Button
 import android.content.Intent
 import android.widget.EditText
 
+/**
+ *
+ */
+
 class ProductAddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_add)
+
+        /**
+         * This script sends the name of product which user is trying to add, and sends it to MainActivity
+         */
 
         val buttonAdd : Button = findViewById(R.id.save)
         val textField : EditText = findViewById(R.id.editProductName)
